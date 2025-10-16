4bddaa866a76b472cc5d05bcab11a5e486a94ecf
Arapov Artem <arapov.artem.2003@mail.ru>
2025-10-16 16:27:06 +0300

    Added user auth with carts/items/users logic

--- src/main/java/ru/arapov/somerestjpafitches/dtos/UpdateCartItemRequest.java
+++
@@ -1,8 +0,0 @@
-package ru.arapov.somerestjpafitches.dtos;
-
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-
-public record UpdateCartItemRequest(
-        @NotNull @Min(0) Integer quantity
-) {}
--- src/main/java/ru/arapov/somerestjpafitches/exceptions/UserExceptions.java
+++
@@ -1,8 +0,0 @@
-package ru.arapov.somerestjpafitches.exceptions;
-
-public class UserExceptions extends RuntimeException {
-
-    public UserExceptions(String message) {
-        super(message);
-    }
-}
